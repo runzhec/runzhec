67cfc16951317377298cfddce05dcc1516128456
runzhec <runzhe.cui.3@gmail.com>
2026-05-26 10:35:35 -0500

    msg update

--- src/pages/NightSky.jsx
+++ src/pages/NightSky.jsx
@@ -132,7 +132,7 @@ export default function NightSky({ isTotal = false }) {
       <div className="spaceship-wrapper">
         <div className="spaceship-scene">
           <div className="banner">
-            <span>good luck!!!</span>
+            <span>hope ur exams went well!!!</span>
           </div>
 
           <div className="spaceship">
