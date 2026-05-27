902174669eae73825052069ac74a08e4ce594973
runzhec <runzhe.cui.3@gmail.com>
2026-05-27 12:51:29 -0500

    msg update

--- src/pages/NightSky.jsx
+++ src/pages/NightSky.jsx
@@ -132,7 +132,7 @@ export default function NightSky({ isTotal = false }) {
       <div className="spaceship-wrapper">
         <div className="spaceship-scene">
           <div className="banner">
-            <span>thinking abt u :0</span>
+            <span>You make life sweeter🍑</span>
           </div>
 
           <div className="spaceship">
